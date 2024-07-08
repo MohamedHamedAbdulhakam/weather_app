92caa7b18d7e5fcfc742c7feb1afab506eee338b
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-08 23:03:26 +0300

    over view about the project

--- lib/widget/weathet_info_body.dart
+++ lib/widget/weathet_info_body.dart
@@ -10,6 +10,35 @@ class WeatherInfoBody extends StatelessWidget {
             horizontal: 16,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Alex',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 32)),
+                const Text('updated at 7 pm', style: TextStyle(fontSize: 32)),
+                SizedBox(
+                  height: 32,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset('assets/cloudy.png'),
+                    const Text('17',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 32)),
+                    Column(
+                      children: [
+                        const Text('max temp 24',
+                            style: TextStyle(fontSize: 16)),
+                        const Text('min temp 16',
+                            style: TextStyle(fontSize: 16)),
+                      ],
+                    )
+                  ],
+                ),
+                const Text('light rain',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 32)),
+              ],
             )));
   }
 }
