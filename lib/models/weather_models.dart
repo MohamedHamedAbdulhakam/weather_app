9cb55a2c4e1f6b125fd4f359b1e83cd2829d7b6c
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-12 23:24:14 +0300

    create weather model part two

--- lib/models/weather_models.dart
+++ lib/models/weather_models.dart
@@ -15,4 +15,16 @@ class WeatherModel {
       required this.maxtemp,
       required this.mintemp,
       required this.weathercondition});
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+      image: json['forecast']['forecastday'][0]['day']['condition']['text'],
+      cityname: json['location']['name'],
+      date: json['current']['last_updated'],
+      temp: json['forecast']['forecastday'][0]['day']['avgtemp_c'],
+      maxtemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+      mintemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+      weathercondition: json['forecast']['forecastday'][0]['day']['condition']
+          ['text'],
+    );
+  }
 }
