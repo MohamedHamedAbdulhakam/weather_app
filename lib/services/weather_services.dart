893b4b13e1f8f1ef2e09873088e1085e552b01ef
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-13 12:25:45 +0300

    handling error

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -7,10 +7,14 @@ class WeatherService {
   final Dio dio;
 
   WeatherService({required this.dio});
-  Future<WeatherModel> getCurrentWeather({required String cityname}) async {
-    Response response = await dio.get(
-        '$domain/forecast.json?key=$apiKey&q=London&days=1&aqi=no&alerts=no');
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
+  Future<WeatherModel?> getCurrentWeather({required String cityname}) async {
+    try {
+      Response response = await dio.get(
+          '$domain/forecast.json?key=$apiKey&q=London&days=1&aqi=no&alerts=no');
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } catch (e) {
+      return null;
+    }
   }
 }
