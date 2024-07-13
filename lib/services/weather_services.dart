9f581abb40d3d912b3e65f48f77eb39518cbecd1
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-13 12:11:18 +0300

    apiKey

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -3,19 +3,13 @@ import 'package:weather_app/models/weather_models.dart';
 
 class WeatherService {
   final String domain = 'http://api.weatherapi.com/v1';
+  final String apiKey = '36e1d34f88db4066933180908241207';
   final Dio dio;
 
   WeatherService({required this.dio});
   Future<WeatherModel> getCurrentWeather({required String cityname}) async {
     Response response = await dio.get(
-        '$domain/forecast.json?key=36e1d34f88db4066933180908241207&q=London&days=1&aqi=no&alerts=no');
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
-  }
-
-  Future<WeatherModel> getForecast({required cityname}) async {
-    Response response = await dio.get(
-        '$domain/forecast.json?key=36e1d34f88db4066933180908241207&q=London&days=1&aqi=no&alerts=no');
+        '$domain/forecast.json?key=$apiKey&q=London&days=1&aqi=no&alerts=no');
     WeatherModel weatherModel = WeatherModel.fromJson(response.data);
     return weatherModel;
   }
