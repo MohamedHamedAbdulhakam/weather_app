78f2be2575fb94d3e4cc10906e1290846adb2973
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-13 12:03:51 +0300

    base url

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,16 +1,21 @@
-import 'dart:nativewrappers/_internal/vm/lib/core_patch.dart';
-import 'dart:ui';
-
 import 'package:dio/dio.dart';
 import 'package:weather_app/models/weather_models.dart';
 
 class WeatherService {
+  final String domain = 'http://api.weatherapi.com/v1';
   final Dio dio;
 
   WeatherService({required this.dio});
-  Future<WeatherModel> getCurrentWeather({required cityname}) async {
+  Future<WeatherModel> getCurrentWeather({required String cityname}) async {
     Response response = await dio.get(
-        'http://api.weatherapi.com/v1/forecast.json?key=36e1d34f88db4066933180908241207&q=London&days=1&aqi=no&alerts=no');
+        '$domain/forecast.json?key=36e1d34f88db4066933180908241207&q=London&days=1&aqi=no&alerts=no');
+    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+    return weatherModel;
+  }
+
+  Future<WeatherModel> getForecast({required cityname}) async {
+    Response response = await dio.get(
+        '$domain/forecast.json?key=36e1d34f88db4066933180908241207&q=London&days=1&aqi=no&alerts=no');
     WeatherModel weatherModel = WeatherModel.fromJson(response.data);
     return weatherModel;
   }
