80c9d049e7df41b369b5c0539fe35a8a9be2f715
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-19 21:01:47 +0300

    integrate cubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubits/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubits/get_weather_state.dart';
 import 'package:weather_app/views/search_view.dart';
 import 'package:weather_app/widget/no_weather_body.dart';
 import 'package:weather_app/widget/weathet_info_body.dart';
@@ -28,7 +31,17 @@ class HomeView extends StatelessWidget {
           )
         ],
       ),
-      body: const NoWheaterBody(),
+      body: BlocBuilder<GetWeatherCubit, WeatherState>(
+        builder: (context, state) {
+          if (state is NoWeatherState) {
+            return const NoWheaterBody();
+          } else if (state is WeatherLoadedState) {
+            return const WeatherInfoBody();
+          } else {
+            return Text("opps there was an error");
+          }
+        },
+      ),
     );
   }
 }
